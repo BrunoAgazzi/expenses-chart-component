ed24c39ced154665dab572e6c8d4e345121279de
Bruno Agazzi <63024223+BrunoAgazzi@users.noreply.github.com>
2022-07-23 17:17:26 -0300

    new getday

--- js/app.js
+++ js/app.js
@@ -1,28 +1,33 @@
+const getData = () => {
+    fetch("../data.json")
+    .then(response => {
+        return response.json();
+    })
+    .then(jsondata => jsondata.map(dat =>{
+        let date = new Date() ;
+        let day = date.getDay();
+        console.log(day)
+        let container = document.getElementById("chart-bars");
+        let div = document.createElement("div");
+        let h3 = document.createElement("h3");
+        let h4 = document.createElement("h4");
+        let bar = document.createElement("div");
+        bar.style.height= `${dat.amount * 2}px`;
+        bar.className= dat.dayNumber === day ? "bar bar-max" :"bar";
+        h4.innerHTML = `$${dat.amount}`;
+        bar.addEventListener("mouseover", function(event){
+            h4.style.display= "block";
+        });
+        bar.addEventListener("mouseout", function(event){
+            h4.style.display= "none";
+        });
+        div.className= "bar-container";
+        h3.innerHTML = dat.day;
+        div.appendChild(h4);
+        div.appendChild(bar);
+        div.appendChild(h3);
+        container.appendChild(div);
+    }));
+}
 
-
-fetch("../data.json")
-.then(response => {
-    return response.json();
-})
-.then(jsondata => jsondata.map(dat =>{
-    let container = document.getElementById("chart-bars");
-    let div = document.createElement("div");
-    let h3 = document.createElement("h3");
-    let h4 = document.createElement("h4");
-    let bar = document.createElement("div");
-    bar.style.height= `${dat.amount * 2}px`;
-    bar.className= dat.day === "wed" ? "bar bar-max" :"bar";
-    h4.innerHTML = `$${dat.amount}`;
-    bar.addEventListener("mouseover", function(event){
-        h4.style.display= "block";
-    });
-    bar.addEventListener("mouseout", function(event){
-        h4.style.display= "none";
-    });
-    div.className= "bar-container";
-    h3.innerHTML = dat.day;
-    div.appendChild(h4);
-    div.appendChild(bar);
-    div.appendChild(h3);
-    container.appendChild(div);
-}));
+getData();
